937b3b6507e25d9933c643d33b6ceb3f5e3a2144
Poohma <panzerpanda@gmail.com>
2021-09-13 00:55:12 +0200

    some fixes and comments, now fits better on a widescreen and shows the bits

--- js/binary.js
+++ js/binary.js
@@ -1,11 +1,13 @@
 
 document.addEventListener("DOMContentLoaded", function(){
 
-    function displayNumber(hour, minute) {//takes in the time 
+    function displayNumber(hour, minute) 
+	{//takes in the time 
 		var binaryh =(hour).toString(2);//converts hour variable to binary 
 		var hlength = binaryh.length;
 		if (hlength <= 5){//pads out with zeros so that the final binary is 11-digits
-		for (var i = (4 - hlength); i >= 0; i--){
+		for (var i = (4 - hlength); i >= 0; i--)
+		{
 			var tmp;
 			tmp = "0" + binaryh;
 			binaryh = tmp;
@@ -24,30 +26,25 @@ document.addEventListener("DOMContentLoaded", function(){
 
 		var binary = binarym + binaryh; //adds them together to get a the 11digit binary  
 		//I fucked up the order of these for some reason and forgot,
-		//document.getElementById("bin").innerHTML = binary; //this is to see the binary, didnt really work since I failed at setting them together
+		document.getElementById("bin").innerHTML = binaryh + binarym; //this is to see the binary, didnt really work since I failed at setting them together
 
-        for (var i = 1; i <= 11; i++) {//changes the color of the pentagram according to the bit
-            if (binary[11-i] == 1) {
-				document.getElementById("layer"+i).style.fill = 'lime'
-            }
-            else if (binary[11-i] == 0) {
-				document.getElementById("layer"+i).style.fill = 'maroon'
-            }
-            else {
-                document.getElementById("layer"+i).style.fill = 'white'//this makes it easy to see if something is wrong
-            }
-        }
-    }
+        for (var i = 1; i <= 11; i++) 
+		{//changes the color of the pentagram according to the bit
+            if (binary[11-i] == 1) {document.getElementById("layer"+i).style.fill = 'lime'}
+            else if (binary[11-i] == 0) {document.getElementById("layer"+i).style.fill = 'maroon'}
+            else {document.getElementById("layer"+i).style.fill = 'white'}//this makes it easy to see if something is wrong
+		} 
+	}
+    
 
 
-   function displayTime() {
+   function displayTime() 
+   {
 
 		var date = new Date();
         displayNumber(date.getHours(), date.getMinutes());
-
-        setTimeout(function() {
-            displayTime()
-        }, 1000);
-    }
-    displayTime();
+        setTimeout(function() {displayTime()}, 1000);
+	}
+    
+	displayTime();
 });
