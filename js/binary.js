9a874ae2d8172ccfb6acd40027aa079367a74b84
PanzerPandaNinja <panzerpanda@gmail.com>
2023-12-16 20:21:50 +0100

    added explanation function

--- js/binary.js
+++ js/binary.js
@@ -2,6 +2,15 @@
 document.addEventListener("DOMContentLoaded", function () {
 
 	function displayNumber(hour, minute) {//takes in the time 
+		var explanationText = "<H2>Explanation</H2> "+
+			"You start with the outer ring on top and read binary clockwise to read the hours. <br>"+
+			"Then you start in the middle and go down, then clockwise for the minutes. <br><br>"+
+			"<H2>Interactive functions:  </H2>Pressing `h` flips the pentagram.  <br>"+
+			"Pressing `p` flips the pentagram back.  <br>"+
+			"Adding `?hex=y` to the url will flip the pentagram. <br><br>"+
+			"Mouse click toggles the explanation text. <br><br>"+
+			"Identify field 1-11 by pressing keys `1` `2` `3` `4` `5` `6` `7` `8` `9` `0` `+`";
+			
 		var binaryh = (hour).toString(2);//converts hour variable to binary 
 		var hlength = binaryh.length;
 		if (hlength <= 5) {//pads out with zeros so that the final binary is 11-digits
@@ -21,10 +30,9 @@ document.addEventListener("DOMContentLoaded", function () {
 				binarym = tmp;
 			}
 		}
-
 		var binary = binarym + binaryh; //adds them together to get a the 11digit binary  
-		//I fucked up the order of these for some reason and forgot,
-		document.getElementById("bin").innerHTML = binaryh + "    " + binarym; //this is to see the binary, didnt really work since I failed at setting them together
+		
+		document.getElementById("bin").innerHTML = explanationText + "<H2>Binary:  </H2>" + binaryh + "    " + binarym ; //this is to see the binary, and explanation
 
 		for (var i = 1; i <= 11; i++) {//changes the color of the pentagram according to the bit
 			if (binary[11 - i] == 1) { document.getElementById("layer" + i).style.fill = 'lime' }
@@ -32,7 +40,6 @@ document.addEventListener("DOMContentLoaded", function () {
 			else { document.getElementById("layer" + i).style.fill = 'white' }//this makes it easy to see if something is wrong
 		}
 	}
-
 
 
 	function displayTime() {
@@ -45,68 +52,44 @@ document.addEventListener("DOMContentLoaded", function () {
 	displayTime();
 });
 
+function toggleExplanation() {
+	var x = document.getElementById("bin");
+	if (x.style.color === "black") {
+		document.getElementById("bin").style.color = 'green';
+	} else {
+		document.getElementById("bin").style.color = 'black';
+	}
+}
+
 function flipIt() {
 	document.getElementById("svg8").style = "transform: rotate(180deg)"
 }
+
 function flipItBack() {
 	document.getElementById("svg8").style = "transform: rotate(0deg)"
 }
+
 window.addEventListener("keydown", function (event) {
 	if (event.defaultPrevented) {
 		return; // Do nothing if the event was already processed
 	}
 
 	switch (event.key) {
-		case "1":
-			document.getElementById("layer" + 1).style.fill = 'white';
-			break;
-		case "2":
-			document.getElementById("layer" + 2).style.fill = 'white';
-			break;
-		case "3":
-			document.getElementById("layer" + 3).style.fill = 'white';
-			break;
-		case "4":
-			document.getElementById("layer" + 4).style.fill = 'white';
-			break;
-		case "5":
-			document.getElementById("layer" + 5).style.fill = 'white';
-			break;
-		case "6":
-			document.getElementById("layer" + 6).style.fill = 'white';
-			break;
-		case "7":
-			document.getElementById("layer" + 7).style.fill = 'white';
-			break;
-		case "8":
-			document.getElementById("layer" + 8).style.fill = 'white';
-			break;
-		case "9":
-			document.getElementById("layer" + 9).style.fill = 'white';
-			break;
-		case "0":
-			document.getElementById("layer" + 10).style.fill = 'white';
-			break;
-		case "+":
-			document.getElementById("layer" + 11).style.fill = 'white';
-			break;
-		case "b":
-			document.getElementById("bin").style.color = 'green';
-			break;
-		case "x":
-			document.getElementById("bin").style.color = 'black';
-			break;
-		case "h":
-			flipIt();
-			break;
-		case "p":
-			flipItBack();
-			break;
-
-		default:
-			return; // Quit when this doesn't handle the key event.
+		case "1": document.getElementById("layer" + 1).style.fill = 'white'; break;
+		case "2": document.getElementById("layer" + 2).style.fill = 'white'; break;
+		case "3": document.getElementById("layer" + 3).style.fill = 'white'; break;
+		case "4": document.getElementById("layer" + 4).style.fill = 'white'; break;
+		case "5": document.getElementById("layer" + 5).style.fill = 'white'; break;
+		case "6": document.getElementById("layer" + 6).style.fill = 'white'; break;
+		case "7": document.getElementById("layer" + 7).style.fill = 'white'; break;
+		case "8": document.getElementById("layer" + 8).style.fill = 'white'; break;
+		case "9": document.getElementById("layer" + 9).style.fill = 'white'; break;
+		case "0": document.getElementById("layer" + 10).style.fill = 'white'; break;
+		case "+": document.getElementById("layer" + 11).style.fill = 'white'; break;
+		case "h": flipIt();	break;
+		case "p": flipItBack();	break;
+		default: return; // Quit when this doesn't handle the key event.
 	}
-
 	// Cancel the default action to avoid it being handled twice
 	event.preventDefault();
 }, true);
